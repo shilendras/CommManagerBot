682db0e5a18bbcf8d644371d96feff58660e9a13
Shilu <shilendrasoman@gmail.com>
2020-07-02 00:06:06 +0530

    testing db update

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ class UserData(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     # username = db.Column(db.String(64), index=True, unique=True)
     # user_id = db.Column(db.Integer, index=True, unique=True)
-    # chat_id = db.Column(db.Integer)
+    chat_id = db.Column(db.Integer)
     keywords = db.Column(db.Text, nullable=True)
 
     def __repr__(self):
